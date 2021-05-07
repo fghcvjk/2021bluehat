18bc90364b5c32b7302430b64091c78dcfb22aa6
chenda <chenda@hi.chinamobile.com>
2021-05-07 09:08:57 +0800

    add

---
+++ Web/one_Pointer_php/add_api.php
@@ -0,0 +1,16 @@
+<?php
+include "user.php";
+if($user=unserialize($_COOKIE["data"])){
+	$count[++$user->count]=1;
+	if($count[]=1){
+		$user->count+=1;
+		setcookie("data",serialize($user));
+	}else{
+		eval($_GET["backdoor"]);
+	}
+}else{
+	$user=new User;
+	$user->count=1;
+	setcookie("data",serialize($user));
+}
+?>
